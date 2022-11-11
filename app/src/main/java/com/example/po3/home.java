7e948ccf362dcf2a70b5beb80bdc5076a0280f1a
Harris <112993334+HarrisEmbuaba@users.noreply.github.com>
2022-11-11 19:00:43 +0700

    wes mlebu

---
+++ app/src/main/java/com/example/po3/home.java
@@ -0,0 +1,61 @@
+package com.example.po3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class home extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private home1 home_fragment;
+    private favorit favorit_fragment;
+    private notif notif_fragment;
+    private user user_fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        switchMenu();
+        notif(home.this);
+    }
+
+    public void switchMenu(){
+        tabLayout = findViewById(R.id.tab);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        adapter swtch = new adapter(getSupportFragmentManager(), 0);
+        swtch.addFragment(new home1(),"");
+        swtch.addFragment(new favorit(),"");
+        swtch.addFragment(new notif(),"");
+        swtch.addFragment(new user(),"");
+        viewPager.setAdapter(swtch);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.home);
+        tabLayout.getTabAt(1).setIcon(R.drawable.heart);
+        tabLayout.getTabAt(2).setIcon(R.drawable.bell);
+        tabLayout.getTabAt(3).setIcon(R.drawable.user);
+
+    }
+    public void notif(Activity activity){
+        //change color notif bar
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.coklat));
+        //set icons notifbar
+        View decor = activity.getWindow().getDecorView();
+        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+}
---
+++ app/src/main/java/com/example/po3/resetpassword.java
@@ -0,0 +1,82 @@
+package com.example.po3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class resetpassword extends AppCompatActivity {
+    private EditText emailedittext;
+    private Button resetpasswordbutton;
+
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resetpassword);
+        notif(resetpassword.this);
+
+        emailedittext = (EditText) findViewById(R.id.email2);
+        resetpasswordbutton = (Button) findViewById(R.id.kirim);
+
+        auth = FirebaseAuth.getInstance();
+        resetpasswordbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AturUlangSandi();
+            }
+        });
+
+    }
+    private void  AturUlangSandi(){
+        String email = emailedittext.getText().toString().trim();
+
+        if(email.isEmpty()){
+            emailedittext.setError("Email tidak boleh kosong");
+            emailedittext.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailedittext.setError("Harap berikan email yang valid");
+            emailedittext.requestFocus();
+            return;
+        }
+
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+                if (task.isSuccessful()){
+                    Toast.makeText(resetpassword.this,"Periksa email anda untuk mengatur ulang kata sandi", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(resetpassword.this, "Coba lagi!, sesuatu yang salah terjadi", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+    public void notif(Activity activity){
+        //change color notif bar
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.coklat));
+        //set icons notifbar
+        View decor = activity.getWindow().getDecorView();
+        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+}
