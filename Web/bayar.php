116815897353c52a2bdaa5b4c211f34ad9e868b6
Rosita1101 <113076386+Rosita1101@users.noreply.github.com>
2022-11-25 19:38:13 +0700

    fungsi riwayat pesanan

--- Web/bayar.php
+++ Web/bayar.php
@@ -154,22 +154,47 @@
             <div class="card-body">
               <h5 class="card-title">Rincian Pesanan</h5>
               <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>ID Bayar</th>
-                                <th>Waktu Bayar</th>
-                                <th>Total</th>
-                                <th>Metode</th>
-                                <th>ID Transaksi</th>
-                            </tr>
-                        </thead>
-                    </table>
-                  </div>
-                </div>
-              </div>
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Transaksi ID</th>
+                                            <th>Waktu Transaksi</th>
+                                            <th>Waktu Pengambilan</th>
+                                            <th>Total</th>
+                                            <th>ID User</th>
+                                        </tr>
+                                    </thead>
+                                    
+                                    <tbody>
+                                    <?php
+                                        include ('koneksi.php');
+        
+                                        $no=1;
+                                        $sql = mysqli_query($mysqli, "SELECT * FROM transaksi");
+                                        while ($data=mysqli_fetch_array($sql)) {
+                                            $transaksi_id = $data['transaksi_id'];
+                                            $waktu_transaksi = $data['waktu_transaksi'];
+                                            $waktu_pengambilan = $data['waktu_pengambilan'];
+                                            $total = $data['total'];
+                                            $id_user = $data['id_user'];
+                                            ?>
+                                            <tr>
+                                                <td><?php echo $no++; ?></td>
+                                                <td><?php echo $transaksi_id; ?></td>
+                                                <td><?php echo $waktu_transaksi; ?></td>
+                                                <td><?php echo $waktu_pengambilan; ?></td>
+                                                <td><?php echo $total; ?></td>
+                                                <td><?php echo $id_user; ?></td>
+                                            </tr>
+                                            <?php ; }?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        
+                
               
               
     <section class="section bayar">
@@ -273,6 +298,8 @@
       </div>
     </section>
 </main><!-- End #main -->
+
+
 
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
