fa587be3017633dc711182460ad5e01af0fc9d1b
Cornelia01 <nelavial01@gmail.com>
2022-11-29 10:15:43 +0700

    Update produk2.php

--- Web/produk2.php
+++ Web/produk2.php
@@ -1,66 +1,64 @@
 <?php
 include ('koneksi.php');
 
-error_reporting(0);
-session_start();
+// error_reporting(0);
+// session_start();
 
-if(isset($_SESSION['id_barang'])){
+if(isset($_SESSION['nama_barang'])){
   header("Location: produk2.php");
-}
 
-$err = "";
-$sukses = "";
-$kode = "";
 
-if(isset($_POST['Submit'])){
-  $id = $_POST['id_barang'];
+// $err = "";
+// $sukses = "";
+// $kode = "";
+
+// $sql1 = "SELECT * FROM `barang`";
+// $all_categories = mysqli_query($koneksi,$sql1);
+
+// $result = mysqli_query($koneksi,$sql1);
+
+if(isset($_POST['Insert'])){
+  //$id = $_POST['id_barang'];
   $nama = $_POST['nama_barang'];
   $deskripsi = $_POST['deskripsi'];
   $harga = $_POST['harga'];
   $stok = $_POST['stok'];
-
+  
   $image = $_FILES['image']['name'];
   $source = $_FILES['image']['tmp_name'];
   $folder = 'gambarproduk/';
-  move_uploaded_file($source, $folder.$image);
-
-  $name = mysqli_real_escape_string($con,$_POST['nama_barang']);
-
-  $id_jenis = mysqli_real_escape_string($con,$_POST['barang_jenis']);
-  $id_ukuran = mysqli_real_escape_string($con,$_POST['ukuran']);
-  $id_warna = mysqli_real_escape_string($con,$_POST['warna']);
   
-  if(!$result->num_rows > 0){
-    $sql = "INSERT INTO `barang`(`nama_barang`,`image`,`deskripsi`,`harga`,`stok`,`id_jenis`,`id_ukuran`,`id_warna`)
-    VALUES ('$nama','$image','$deskripsi','$harga','$stok','$id_jenis','$id_ukuran','$id_warna')";
+  move_uploaded_file($source, $folder.$image);
+  
+  $nama = mysqli_real_escape_string($koneksi,$_POST['nama_barang']);
 
-    $result = mysqli_query($koneksi,$sql);
+  $id_jenis = mysqli_real_escape_string($koneksi,$_POST['barang_jenis']);
+  $id_ukuran = mysqli_real_escape_string($koneksi,$_POST['ukuran']);
+  $id_warna = mysqli_real_escape_string($koneksi,$_POST['warna']);
+  
+  $sql2 = "INSERT INTO `barang`(`nama_barang`,`image`,`deskripsi`,`harga`,`stok`,`id_jenis`,`id_ukuran`,`id_warna`)
+  VALUES ('$nama','$image','$deskripsi','$harga','$stok','$id_jenis','$id_ukuran','$id_warna')";
+  
+  $result = mysqli_query($koneksi,$sql2);
 
-    if($result){
-      echo "<script>alert('Barang berhasil ditambahkan!')</script>";
-      
-      $nama = "";
-      $image = "";
-      $deskripsi = "";
-      $harga = "";
-      $stok = "";
-      $kategori = "";
-      $ukuran = "";
-      $warna = "";
-      $_POST['nama_barang'] = "";
-      $_POST['image'] = "";
-      $_POST['deskripsi'] = "";
-      $_POST['harga'] = "";
-      $_POST['stok'] = "";
-      $_POST['barang_jenis'] = "";
-      $_POST['ukuran'] = "";
-      $_POST['warna'] = "";
-    } else {
-      echo "<script>alert('Barang gagal ditambahkan!')</script>";
-    }
+  echo "<script>
+  eval(Location='produk2.php');
+  alert('Barang berhasil ditambahkan!');
+  </script>";
 
+//   if(!$result->num_rows > 0){
     
-  }
+    // $result = mysqli_query($koneksi,$sql);
+
+    // if($result){
+    
+      
+    } else {
+      echo "<script>
+      eval(Location='produk2.php');
+      alert('Barang gagal ditambahkan!');
+      </script>";
+    }
 }
 
 ?>
@@ -258,7 +256,7 @@ if(isset($_POST['Submit'])){
                     <div class="col-md-1"></div>
                     <label for="kategori" class="col-sm-1 col-form-label">Kategori</label>
                     <div class="col-md-4">
-                      <select type="option" name="jenis_barang" class="form-select">
+                      <select type="option" name="barang_jenis" class="form-select">
                       <?php
                             $sql = "SELECT * FROM jenis_barang";
                             $all_categories = mysqli_query($mysqli, $sql);
@@ -297,7 +295,7 @@ if(isset($_POST['Submit'])){
                           <button type="reset" class="btn btn-primary">Clear</button>
                           <button type="submit" class="btn btn-primary">Update</button>
                           <button type="reset" class="btn btn-primary">Delete</button>
-                          <button type="submit" name="Submit" class="btn btn-primary">Input</button>
+                          <button type="submit" name="Insert" class="btn btn-primary">Input</button>
                         </div>
                       </div>
                     </div>
@@ -335,7 +333,7 @@ if(isset($_POST['Submit'])){
                             RIGHT JOIN jenis_barang ON barang.id_jenis=jenis_barang.id_jenis 
                             RIGHT JOIN jenis_ukuran ON barang.id_ukuran=jenis_ukuran.id_ukuran 
                             RIGHT JOIN jenis_warna ON barang.id_warna=jenis_warna.id_warna 
-                            WHERE stok > 0;";
+                            WHERE stok > 0 ";
                                 if ($result = $mysqli->query($query)) {
                                     while ($row = $result->fetch_assoc()) {
                                         $field1name = $row["id_barang"];
