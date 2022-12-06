a2ff39566dbec98f1f437e6c8eeece63abe87339
RizkyJein <e41212346@student.polije.ac.id>
2022-12-06 11:15:33 +0700

    update edit

--- Web/editPerluKirim.php
+++ Web/editPerluKirim.php
@@ -1,25 +1,101 @@
 <?php 
 
-require ('koneksi.php');
-include ('editPerluKirim.html');
+// require ('koneksi.php');
+// include ('editPerluKirim.html');
 
 
-if (isset($_POST['Edit Status'])) {
-    $id = $_GET['transaksi_id'];
-    $status = $_POST['status'];
- 
-    $sql = "UPDATE transaksi set status='$status'";
-    $result = mysqli_query($conn, $sql);
-    $check = mysqli_num_rows($result);
-    if ($check > 0) {
-        $row = mysqli_fetch_assoc($result);
-        $_SESSION['id'] = $row['transaksi_id'];
-        $_SESSION['status'] = $row['status'];
-        echo "<script>alert('Status berhasil diupdate!')</script>";
-        header("Location: pesan2.php");
-    } else {
-        echo "<script>alert('Status gagal diupdate!')</script>";
+// if (isset($_POST['Edit Status'])) {
+//     $conn->autocommit(false);
+//     try{
+//         $id = $_POST['transaksi_id'];
+//         $status = $_POST['status'];
+
+//         if ($status=="Dikirim"){
+//             $conn->query("UPDATE transaksi SET status = '$status' WHERE transaksi_id = '$idTransaksi'");
+//             $conn->commit();
+//             $response['message'] = "Edit berhasil";
+//             echo "<script>
+//             alert('Update status berhasil!');
+//             </script>";
+//         }else{
+//             $conn->query("UPDATE transaksi SET status = '$status' WHERE transaksi_id = '$idTransaksi'");
+//             $conn->commit();
+//             $response['message'] = "Edit berhasil";
+//             echo "<script>
+//             alert('Ubah Barang Sukses!'); 
+//             </script>";
+//         }
+//     } catch(Exception $e) {
+//         $conn->rollback();
+//         $response['message'] = $e->getMessage();
+//         echo "<script>
+//         "
+//     }
+    
+// }
+
+// // include('koneksi.php');
+
+// // if (isset($_POST['Edit'])) {
+// //     $conn->autocommit(false);
+// //     try{
+// //         $idTransaksi = $_POST['transaksi_id'];
+//         $status = $_POST['status'];
+
+//         if ($status=="Dikirim"){
+//             $conn->query("UPDATE transaksi SET status = '$status' WHERE transaksi_id = '$idTransaksi'");
+//             $conn->commit();
+//             $response['message'] = "Edit berhasil";
+//             echo "<script>
+//             alert('Update status berhasil!'); 
+//             </script>";
+//         }else{
+//             $conn->query("UPDATE transaksi SET status = '$status' WHERE transaksi_id = '$idTransaksi'");
+//             $conn->commit();
+//             $response['message'] = "Edit berhasil";
+//             echo "<script>
+//             alert('Ubah Barang Sukses!'); 
+//             </script>";
+//         }
+//     }catch(Exception $e){
+//         $conn->rollback();
+//         $response['message'] = $e->getMessage();
+//         echo "<script>
+//         alert('Ubah Barang Gagal!'); 
+//         </script>";
+//     }
+// }
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "project3";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (isset($_POST['Edit'])) {
+    $conn->autocommit(false);
+    try{
+        $idTransaksi = $_POST['transaksi_id'];
+        $status = $_POST['status'];
+        
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = "UPDATE transaksi SET status='Dikirim' WHERE status='$status'";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "<script> Record updated successfully </script>";
+        } else {
+            echo "<script> Error updating record: </script>" . mysqli_error($conn);
+        }
+    } catch (Exception $e){
+        
     }
 }
+
+mysqli_close($conn);
 
 ?>
